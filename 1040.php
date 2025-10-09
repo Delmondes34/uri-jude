5c90a6ee6c3aa54ee81118ec60d69085316e2239
Delmondes34 <gustavodel04@gmail.com>
2025-10-09 16:36:03 -0300

    tetativa de resoluçao do teste 1040

---
+++ 1040.php
@@ -0,0 +1,26 @@
+<?php
+
+list($n1, $n2, $n3, $n4) = array_map('floatval', explode(' ', trim(fgets(STDIN))));
+
+$media = (($n1 * 2) + ($n2 * 3) + ($n3 * 4) + ($n4 * 1)) / 10;
+
+echo "Media:" . number_format($media, 1, '.', '') . PHP_EOL;
+
+if ($media >= 7.0) {
+    echo "Aluno aprovado." . PHP_EOL;;
+} elseif ($media < 5.0) {
+    echo "Aluno reprovado." . PHP_EOL;;
+} else {
+    echo "Aluno em exame." . PHP_EOL;;
+    $notaDoExa = floatval(trim(fgets(STDIN)));
+    echo "Nota do exame: " . number_format($notaDoExa, 1, '.', '') . PHP_EOL;
+
+    $mediaFinal = ($notaDoExa + $media) / 2;
+
+    if ($mediaFinal >= 5) {
+        echo "Aluno aprovado.\n";
+    } else {
+        echo "Aluno reprovado.\n";
+    }
+    echo "Media final: " . number_format($mediaFinal, 1, '.', '') . PHP_EOL;
+}
