57dbba4690ec4b8b69959c65d28adc5ac4956346
Delmondes34 <gustavodel04@gmail.com>
2025-10-16 13:43:31 -0300

    resolução do desafio 1064

---
+++ desafio 1/1064.php
@@ -0,0 +1,19 @@
+<?php
+$positive_count = 0;
+$positive_sum = 0;
+
+for ($i = 0; $i < 6; $i++) {
+    $number = floatval(trim(fgets(STDIN))); // Read each number as a float
+
+    if ($number > 0) {
+        $positive_count++;
+        $positive_sum += $number;
+    }
+}
+
+echo $positive_count . " valores positivos\n";
+
+if ($positive_count > 0) {
+    $average = $positive_sum / $positive_count;
+    echo number_format($average, 1, '.', '') . "\n"; // Format average to one decimal place
+}
