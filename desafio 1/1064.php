0107a8a551e759eb2807cbc972c6507983ded71f
Delmondes34 <gustavodel04@gmail.com>
2025-10-16 13:46:19 -0300

    resolução do desafio 1064

--- desafio 1/1064.php
+++ desafio 1/1064.php
@@ -3,8 +3,7 @@ $positive_count = 0;
 $positive_sum = 0;
 
 for ($i = 0; $i < 6; $i++) {
-    $number = floatval(trim(fgets(STDIN))); // Read each number as a float
-
+    $number = floatval(trim(fgets(STDIN)));
     if ($number > 0) {
         $positive_count++;
         $positive_sum += $number;
@@ -15,5 +14,5 @@ echo $positive_count . " valores positivos\n";
 
 if ($positive_count > 0) {
     $average = $positive_sum / $positive_count;
-    echo number_format($average, 1, '.', '') . "\n"; // Format average to one decimal place
+    echo number_format($average, 1, '.', '') . "\n";
 }
